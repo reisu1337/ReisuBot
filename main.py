0cb7d2a61dcc3aa644876044164105910a1fe8eb
reisu1337 <73112598+reisu1337@users.noreply.github.com>
2021-07-21 01:05:44 +0100

    Added missing files

---
+++ main.py
@@ -0,0 +1,40 @@
+from discord import client, guild
+from dotenv import load_dotenv
+import os
+import discord
+from discord.ext import commands
+from discord.utils import get
+
+client = commands.Bot(command_prefix="!")
+
+@client.command()
+async def pronouns(ctx, pronouns):
+    message = ctx.message.content
+    splitMessage = message.split(" ")
+    user = ctx.message.author
+    if "/" in pronouns:
+        if get(ctx.guild.roles, name=pronouns):
+            role = discord.utils.get(user.guild.roles, name=pronouns)
+            await discord.Member.add_roles(user, role)
+            await ctx.send("Pronouns added!")
+        else:
+            await ctx.send("These pronouns haven't been added yet, ask a mod to add them for you!")
+    else:
+        await ctx.send("Make sure your pronouns are formatted as \"a/b\"!")
+
+@client.command()
+@commands.has_role("Mod")
+async def addPronouns(ctx, pronouns):
+    if get(ctx.guild.roles, name=pronouns):
+        await ctx.send("Pronouns are already available!")
+    else:
+        await ctx.guild.create_role(name=pronouns)
+        await ctx.send(f"Pronouns \"{pronouns}\" have been added!")
+
+
+
+
+
+
+load_dotenv(".env")
+client.run(os.getenv("TOKEN"))
